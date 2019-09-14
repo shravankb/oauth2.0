fc2f354be85b865e1ba75b7c9696c2e8274de9fc
shravankb <shravan.kumar@nineleaps.com>
2019-09-14 10:06:40 +0530

    googleStrategy setup

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -5,10 +5,18 @@ passport.use(
     new GoogleStrategy({
     //options for strategy
 
+    clientID:'131756318955-smc4da89vpr0im56tqs096c6pvd2okbb.apps.googleusercontent.com',
+    clientSecret:'PbanrETnlWGo6zQH9_-ilWfF'
+
 }), ()=>{
 //passport callback function
 
 })
+
+
+
+
+
 
 // passport.use(
 //     new GoogleStrategy({
