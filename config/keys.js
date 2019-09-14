22ffdaf87f6db8d20bb84cf1cf2d3b7174bee4bc
shravankb <shravan.kumar@nineleaps.com>
2019-09-14 10:47:47 +0530

    securingGoogleKeys

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// add this file to .gitignore
+
+module.exports={
+    google:{
+        clientID:'131756318955-smc4da89vpr0im56tqs096c6pvd2okbb.apps.googleusercontent.com',
+        clientSecret:'PbanrETnlWGo6zQH9_-ilWfF'
+    }
+};
